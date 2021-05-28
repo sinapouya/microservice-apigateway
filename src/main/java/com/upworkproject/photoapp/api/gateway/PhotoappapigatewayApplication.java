3bc8ef44817e34d66a28c8398caff30c7770c3e3
s.pouya <s.pouya@pinket.ir>
2021-05-28 22:44:28 +0430

    first commit

---
+++ src/main/java/com/upworkproject/photoapp/api/gateway/PhotoappapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.upworkproject.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappapigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upworkproject/photoapp/api/gateway/PhotoappapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upworkproject.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
